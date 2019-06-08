68c70e187ac51acd89c8ba1d701b9d8e154188e7
desoga <done2stay@gmail.com>
2019-06-08 15:38:03 +0100

    made changes

--- script.js
+++ script.js
@@ -2,38 +2,37 @@
 const searchcountry = async searchBox => {
   const res = await fetch('../data/countries.json');
   const countries = await res.json();
-  // console.log(countries);
 
-  //Get Match Of Entered Data
-  let matches = countries.filter(country => {
+  //Get Entered Data
+  let fits = countries.filter(country => {
     const regex = new RegExp(`^${searchBox}`, 'gi');
     return country.name.match(regex) || country.abbr.match(regex);
   });
 
   if (searchBox.length === 0) {
-    matches = [];
+    fits = [];
     countryList.innerHTML = '';
   }
-  // console.log(matches);
-  outputHtml(matches);
+
+  outputHtml(fits);
 };
 
 // show results in HTML
-const outputHtml = matches => {
-  if (matches.length > 0) {
-    const html = matches
+const outputHtml = fits => {
+  if (fits.length > 0) {
+    const html = fits
       .map(
-        match => `
+        fit => `
      <div class="row">
      <div class="col s12">
        <div class="card  grey darken-4 darken-1">
          <div class="card-content white-text">
-           <h4 class="card-title m1">${match.name} (${
-          match.abbr
-        })<span class="blue-text m-4"> ${match.capital}</span></h4>
+           <h4 class="card-title m1">${fit.name} (${
+          fit.abbr
+        })<span class="blue-text m-4"> ${fit.capital}</span></h4>
         <div class="card-action">
         <a>Country Code :</a>
-        <a>${match.phoneCode}</a>
+        <a>${fit.phoneCode}</a>
       </div>
          </div>
        </div>
@@ -42,7 +41,7 @@ const outputHtml = matches => {
      `
       )
       .join('');
-    // console.log(html);
+
     document.getElementById('countryList').innerHTML = html;
   }
 };
